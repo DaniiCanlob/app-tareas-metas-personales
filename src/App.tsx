8252e1ea4ef2734407475f85df7a01ec86485fed
Jesus Cantillo <danii.20151230@gmail.com>
2024-10-31 14:36:54 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className=' h-screen w-screen'>
+        <h1>app-todo-goals</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
